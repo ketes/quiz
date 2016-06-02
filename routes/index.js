f0cc01c2ab73049595ca9d8dbe6ec19124eca03a
ketes <m.romerod@alumnos.upm.es>
2016-06-02 04:53:13 +0200

    Pagina de creditos

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ var quizController = require('../controllers/quiz_controller');
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+router.get('/author', function(req, res, next) {
+  res.render('author');
+});
 
 router.get('/question', quizController.question);
 router.get('/check',    quizController.check);
