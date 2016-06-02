b4f5a0e83f1c6d072b124f5d6ad9ea41f3741e57
ketes <m.romerod@alumnos.upm.es>
2016-06-02 17:40:50 +0200

    Tema 24: Moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,7 @@ router.get('/author', function(req, res, next) {
 // Autoload de parametros
 router.param('quizId', quizController.load);  // autoload :quizId
 router.param('userId', userController.load);  // autoload :userId
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/session',    sessionController.new);     // formulario login
@@ -44,7 +45,14 @@ router.get('/quizzes/:quizId(\\d+)/edit',  sessionController.loginRequired, quiz
 router.put('/quizzes/:quizId(\\d+)',       sessionController.loginRequired, quizController.update);
 router.delete('/quizzes/:quizId(\\d+)',    sessionController.loginRequired, quizController.destroy);
 
-router.get('/quizzes/:quizId(\\d+)/comments/new',  sessionController.loginRequired, commentController.new);
-router.post('/quizzes/:quizId(\\d+)/comments',     sessionController.loginRequired, commentController.create);
+// Definición de rutas de comentarios
+router.get('/quizzes/:quizId(\\d+)/comments/new',  sessionController.loginRequired, 
+	                                               commentController.new);
+router.post('/quizzes/:quizId(\\d+)/comments',     sessionController.loginRequired, 
+	                                               commentController.create);
+router.put('/quizzes/:quizId(\\d+)/comments/:commentId(\\d+)/accept', 
+	                                               sessionController.loginRequired, 
+	                                               commentController.accept);
+
 
 module.exports = router;
