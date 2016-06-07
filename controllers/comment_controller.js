c32f110231b5df96b7c52ec837fffc7b88774594
ketes <m.romerod@alumnos.upm.es>
2016-06-07 20:11:26 +0200

    P13: Autores de los comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -28,12 +28,14 @@ exports.new = function(req, res, next) {
 
 // POST /quizes/:quizId/comments
 exports.create = function(req, res, next) {
+  var authorId = req.session.user && req.session.user.id || 0;
   var comment = models.Comment.build(
       { text:   req.body.comment.text,          
-        QuizId: req.quiz.id
+        QuizId: req.quiz.id,
+	AuthorId: authorId
       });
 
-  comment.save()
+  comment.save({fields: ["text", "QuizId", "AuthorId"]})
     .then(function(comment) {
       req.flash('success', 'Comentario creado con éxito.');
       res.redirect('/quizzes/' + req.quiz.id);
--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -10,8 +10,10 @@ var cloudinary_image_options = { crop: 'limit', width: 200, height: 200, radius:
 
 // Autoload el quiz asociado a :quizId
 exports.load = function(req, res, next, quizId) {
-	models.Quiz.findById(quizId, { include: [ models.Comment, models.Attachment ] })
-  		.then(function(quiz) {
+	models.Quiz.findById(quizId, {
+		      include: [{model:models.Attachment},
+				{model:models.Comment,include:[{model:models.User, as:'Author'}]
+			}]})
       		if (quiz) {
         		req.quiz = quiz;
         		next();
